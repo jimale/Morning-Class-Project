470d0a81fe34c6219762c42213c004950f9e92dc
Jimale <jimcalecabdi@gmail.com>
2021-03-16 09:54:36 +0300

    Added Spinner

--- app/src/main/java/com/tiriig/firstapplication/MainActivity.kt
+++ app/src/main/java/com/tiriig/firstapplication/MainActivity.kt
@@ -3,16 +3,22 @@ package com.tiriig.firstapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val spinner = findViewById<Spinner>(R.id.mySpinner)
+
+        val languages = arrayOf("Select language","Kotlin","Java","Node js","Php")
+
+        val data = ArrayAdapter(this,android.R.layout.simple_spinner_item,languages)
+
+        spinner.adapter = data
+
 
         val mybutton = findViewById<Button>(R.id.saveUser)
         val  editText = findViewById<EditText>(R.id.myEditText)
