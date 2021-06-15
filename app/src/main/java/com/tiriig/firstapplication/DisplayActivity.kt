5074a530e7004900002f5d9ac92d80700ed500b5
Jimale <jimcalecabdi@gmail.com>
2021-06-15 09:46:12 +0300

    Added RecyclerView

--- app/src/main/java/com/tiriig/firstapplication/DisplayActivity.kt
+++ app/src/main/java/com/tiriig/firstapplication/DisplayActivity.kt
@@ -3,12 +3,31 @@ package com.tiriig.firstapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 
 class DisplayActivity : AppCompatActivity() {
+
+    private val adapter = DisplayAdapter()
+    lateinit var recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display)
+
+        setRecyclerView()
+    }
+
+    private fun setRecyclerView() {
+        val users = ArrayList<User>()
+        users.add(User(1,"Mohamed Ali","46282932","mohamed@gmail.com"))
+        users.add(User(2,"Ahmed Jama","72322932","ahmed@gmail.com"))
+        users.add(User(3,"Ali Ali","34282932","ali@gmail.com"))
+
+
+
+        recyclerView = findViewById(R.id.displayRecyclerView)
+        adapter.submitList(users)
+        recyclerView.adapter = adapter
 
     }
 }
--- app/src/main/java/com/tiriig/firstapplication/DisplayAdapter.kt
+++ app/src/main/java/com/tiriig/firstapplication/DisplayAdapter.kt
@@ -1,7 +1,48 @@
 package com.tiriig.firstapplication
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
 
-class DisplayAdapter: ListAdapter<User,DisplayAdapter.ViewHolder> {
+class DisplayAdapter : ListAdapter<User,DisplayAdapter.ViewHolder>(DisplayDiffCallBack()){
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DisplayAdapter.ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_user,parent,false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: DisplayAdapter.ViewHolder, position: Int) {
+        val data = getItem(position)
+        holder.bind(data)
+    }
+
+    inner class ViewHolder(view:View): RecyclerView.ViewHolder(view){
+
+
+        val name : TextView = itemView.findViewById(R.id.name)
+        val phone : TextView = itemView.findViewById(R.id.phone)
+        val email : TextView = itemView.findViewById(R.id.email)
+
+        fun bind(user: User){
+            name.text = user.name
+            phone.text = user.phone
+            email.text = user.email
+        }
+
+    }
+
+    class DisplayDiffCallBack: DiffUtil.ItemCallback<User>() {
+        override fun areItemsTheSame(oldItem: User, newItem: User): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: User, newItem: User): Boolean {
+            return oldItem == newItem
+        }
+
+    }
 }
